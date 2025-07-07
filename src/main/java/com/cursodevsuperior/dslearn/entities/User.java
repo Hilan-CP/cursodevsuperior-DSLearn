62a640931f671ad695cc28dd270f43eb87ec5b32
Hilan <hilan.paganini@gmail.com>
2025-07-07 14:28:09 -0300

    Projeto criado

---
+++ src/main/java/com/cursodevsuperior/dslearn/entities/User.java
@@ -0,0 +1,96 @@
+package com.cursodevsuperior.dslearn.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String email;
+	private String password;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role",
+		joinColumns = @JoinColumn(name = "user_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private List<Role> roles = new ArrayList<>();
+	
+	public User() {
+	}
+
+	public User(Long id, String name, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_role(authority) VALUES ('ROLE_STUDENT');
+INSERT INTO tb_role(authority) VALUES ('ROLE_INSTRUCTOR');
+INSERT INTO tb_role(authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user(name, email, password) VALUES ('Bob','bob@email.com','123456');
+INSERT INTO tb_user(name, email, password) VALUES ('John','john@email.com','123456');
+INSERT INTO tb_user(name, email, password) VALUES ('Mary','mary@email.com','123456');
+
+INSERT INTO tb_user_role(user_id, role_id) VALUES (1,1);
+INSERT INTO tb_user_role(user_id, role_id) VALUES (2,1);
+INSERT INTO tb_user_role(user_id, role_id) VALUES (2,2);
+INSERT INTO tb_user_role(user_id, role_id) VALUES (3,2);
+INSERT INTO tb_user_role(user_id, role_id) VALUES (3,3);
